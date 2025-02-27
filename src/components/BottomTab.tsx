26fc273d057ff357b7df2ffd122622f6d3d987c8
Sanghoon Lee <dltkddnr03131@gmail.com>
2025-02-28 04:16:07 +0900

    change bottom tab

--- src/components/BottomTab.tsx
+++ src/components/BottomTab.tsx
@@ -1,3 +1,4 @@
+import { useActions } from "@stackflow/react";
 import React from "react";
 
 import IconChatting from "../assets/IconChatting";
@@ -7,39 +8,67 @@ import IconProfile from "../assets/IconProfile";
 import IconSell from "../assets/IconSell";
 import * as css from "./BottomTab.css";
 
-const BottomTab: React.FC = () => (
-  <div className={css.container}>
-    <button type="button" className={css.button}>
-      <div className={css.buttonIcon}>
-        <IconHome />
-      </div>
-      <div className={css.buttonLabel}>Home</div>
-    </button>
-    <button type="button" className={css.button}>
-      <div className={css.buttonIcon}>
-        <IconMenu />
-      </div>
-      <div className={css.buttonLabel}>Categories</div>
-    </button>
-    <button type="button" className={css.button}>
-      <div className={css.buttonIcon}>
-        <IconSell />
-      </div>
-      <div className={css.buttonLabel}>Sell</div>
-    </button>
-    <button type="button" className={css.button}>
-      <div className={css.buttonIcon}>
-        <IconChatting />
-      </div>
-      <div className={css.buttonLabel}>Chats</div>
-    </button>
-    <button type="button" className={css.button}>
-      <div className={css.buttonIcon}>
-        <IconProfile />
-      </div>
-      <div className={css.buttonLabel}>My</div>
-    </button>
-  </div>
-);
+const BottomTab: React.FC = () => {
+    const { push } = useActions();
+
+    return (
+        <div className={css.container}>
+            <button
+                type="button"
+                className={css.button}
+                onClick={() => push("Main", { transitionType: "horizontal" }, { animate: true })}
+            >
+                <div className={css.buttonIcon}>
+                    <IconHome />
+                </div>
+                <div className={css.buttonLabel}>Home</div>
+            </button>
+
+            <button
+                type="button"
+                className={css.button}
+                onClick={() => push("MapTab", { transitionType: "horizontal" }, { animate: true })}
+            >
+                <div className={css.buttonIcon}>
+                    <IconMenu />
+                </div>
+                <div className={css.buttonLabel}>Map</div>
+            </button>
+
+            <button
+                type="button"
+                className={css.button}
+                onClick={() => push("Gift", { transitionType: "horizontal" }, { animate: true })}
+            >
+                <div className={css.buttonIcon}>
+                    <IconSell />
+                </div>
+                <div className={css.buttonLabel}>Gift</div>
+            </button>
+
+            <button
+                type="button"
+                className={css.button}
+                onClick={() => push("Chats", { transitionType: "horizontal" }, { animate: true })}
+            >
+                <div className={css.buttonIcon}>
+                    <IconChatting />
+                </div>
+                <div className={css.buttonLabel}>Chats</div>
+            </button>
+
+            <button
+                type="button"
+                className={css.button}
+                onClick={() => push("My", { transitionType: "horizontal" }, { animate: true })}
+            >
+                <div className={css.buttonIcon}>
+                    <IconProfile />
+                </div>
+                <div className={css.buttonLabel}>My</div>
+            </button>
+        </div>
+    );
+};
 
 export default BottomTab;
